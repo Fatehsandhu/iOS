aa1df550da80263dec3d4a08fc587a012c179f3a
Fateh Sandhu <fatehsandhu@Fatehs-MacBook-Pro.local>
2017-02-20 21:59:16 -0500

    Initial Commit

---
+++ Classes/EntityClasses.swift
@@ -0,0 +1,16 @@
+//
+//  EntityClasses.swift
+//  Classes
+//
+//  Created by Fateh Sandhu on 2017-02-20.
+//  Copyright © 2017 School of ICT, Seneca College. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Program)
+class Program: NSManagedObject {}
+
+@objc(Course)
+class Course: NSManagedObject {}
